fa8d39fc92f95443935efdb53a9cd3c36f31553d
Iuri Albuquerque Reis <iareis@gmail.com>
2020-12-28 00:41:16 -0300

    Aula Javascript

---
+++ Aula.Javascript/js/main.js
@@ -0,0 +1,13 @@
+/* var fruta = { nome: "maça", cor: "vermelha" };
+console.log(fruta);
+console.log(fruta.cor);
+alert(fruta.nome); */
+
+var frutas = [
+    { nome: "maça", cor: "vermelha" },
+    { nome: "uva", cor: "roxa" },
+    { nome: "banana", cor: "verde" },
+    { nome: "Limão", cor: "verde" },
+];
+console.log(frutas);
+alert(frutas[1].nome);
---
+++ Aula.Javascript/js/main1.js
@@ -0,0 +1,36 @@
+/* var nome = "Iuri Albuquerque Reis";
+var idade = 50;
+var idade2 = 51;
+
+alert("Bem vindo " + nome);
+alert(nome + " tem " + idade + " anos");
+
+alert(idade + idade2);
+
+var frase = "Japão é o melhor time do mundo";
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+
+alert(frase.replace("Japão", "Brasil"));
+alert(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+############################### */
+
+var lista = [
+    "maça",
+    "pera",
+    "laranja",
+]; /* os elemento são enumerado partindo do ZERO */
+lista.push("uva"); /* push para cololar */
+lista.pop(); /* pop para retirar */
+console.log(lista);
+console.log(lista[1]);
+alert(lista[2]);
+
+console.log(lista.length); /* para dizer quantos elementos tem na lista */
+console.log(lista.reverse()); /* inverte a lista */
+console.log(lista.toString());
+console.log(lista.join(" -- "));
