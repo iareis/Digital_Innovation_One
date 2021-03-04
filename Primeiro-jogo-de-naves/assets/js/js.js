cc333fa0bd927b39b5647bcda736519227bc32b9
Iuri Albuquerque Reis <iareis@gmail.com>
2021-03-03 23:58:25 -0300

    js.js

---
+++ Primeiro-jogo-de-naves/assets/js/js.js
@@ -0,0 +1,13 @@
+function start() {
+    $("$inicio").hide(); // os comando aqui são aceitos assim devido ao jQuery
+
+    $("$fundoGame").append("<div id='jogador' class='anima1'></div>");
+    $("$fundoGame").append("<div id='inimigo1'></div>");
+    $("$fundoGame").append("<div id='inimigo2'></div>");
+    $("$fundoGame").append("<div id='amigo'></div>");
+}
+
+
+class='anima2'
+class='anima3'
+
